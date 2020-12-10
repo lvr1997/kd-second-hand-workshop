a99078e679dff6752c75ab71c5695caf1a55f76f
lvr1997 <lvr1997@qq.com>
2020-12-10 17:37:53 +0800

    更新至Spring5.x版本

--- src/main/java/com/springmvc/web/AccessInterceptor.java
+++ src/main/java/com/springmvc/web/AccessInterceptor.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class AccessInterceptor implements HandlerInterceptor {
 
+	@Override
 	public boolean preHandle(
 			HttpServletRequest request, 
 			HttpServletResponse response, 
@@ -33,6 +34,7 @@ public class AccessInterceptor implements HandlerInterceptor {
 		return true;
 	}
 
+	@Override
 	public void postHandle(
 			HttpServletRequest request, 
 			HttpServletResponse response, 
@@ -40,6 +42,7 @@ public class AccessInterceptor implements HandlerInterceptor {
 			ModelAndView modelAndView) throws Exception {
 	}
 
+	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 	}
